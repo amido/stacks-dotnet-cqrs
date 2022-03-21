935810a40037964f0a3b9537924cd0e819c67d9e
Pavel Danov <pavel.danov@outlook.com>
2022-03-21 15:17:05 +0000

    Update domain aggregate root with DynamoDB specific attributes

--- src/api/xxAMIDOxx.xxSTACKSxx.Domain/MenuAggregateRoot/Aggregate/Menu.cs
+++ src/api/xxAMIDOxx.xxSTACKSxx.Domain/MenuAggregateRoot/Aggregate/Menu.cs
@@ -11,10 +11,154 @@ using xxAMIDOxx.xxSTACKSxx.Domain.Events;
 using xxAMIDOxx.xxSTACKSxx.Domain.MenuAggregateRoot.Exceptions;
 using Amido.Stacks.Domain.Events;
 using Amido.Stacks.DynamoDB.Converters;
+using xxAMIDOxx.xxSTACKSxx.Domain.MenuAggregateRoot.Entities.Converters;
 
 namespace xxAMIDOxx.xxSTACKSxx.Domain
 {
-#if (CosmosDb || InMemoryDb)
+#if (DynamoDb)
+    [DynamoDBTable("Menu")]
+    public class Menu
+    {
+        [JsonProperty("Categories")]
+        [DynamoDBIgnore]
+        private List<Category> categories;
+
+        // DynamoDB needs an empty constructor. In a real project you'll probably wrap your domain obj in a Dynamo DTO
+        public Menu()
+        {
+            this.categories = categories ?? new List<Category>();
+        }
+
+        public Menu(Guid id, string name, Guid tenantId, string description, bool enabled, List<Category> categories = null)
+        {
+            Id = id;
+            Name = name;
+            TenantId = tenantId;
+            Description = description;
+            this.categories = categories ?? new List<Category>();
+            Enabled = enabled;
+        }
+
+        [DynamoDBHashKey(typeof(DynamoDbGuidConverter))]
+        public Guid Id { get; private set; }
+
+        [DynamoDBProperty]
+        public string Name { get; private set; }
+
+        [DynamoDBProperty(typeof(DynamoDbGuidConverter))]
+        public Guid TenantId { get; private set; }
+
+        [DynamoDBProperty]
+        public string Description { get; private set; }
+
+        [JsonIgnore]
+        [DynamoDBProperty(AttributeName = "Categories", Converter = typeof(DynamoDbCategoryConverter))]
+        public IReadOnlyList<Category> Categories { get => categories?.AsReadOnly(); private set => categories = value.ToList(); }
+
+        [DynamoDBProperty]
+        public bool Enabled { get; private set; }
+
+        public void Update(string name, string description, bool enabled)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Enabled = enabled;
+
+            Emit(new MenuChanged());//TODO: Pass the event data
+        }
+
+        public void AddCategory(Guid categoryId, string name, string description)
+        {
+            if (categories.Any(c => c.Name == name))
+                CategoryAlreadyExistsException.Raise(Id, name);
+
+            categories.Add(new Category(categoryId, name, description));
+
+            Emit(new CategoryCreated());//TODO: Pass the event data
+        }
+
+        public void UpdateCategory(Guid categoryId, string name, string description)
+        {
+            var category = GetCategory(categoryId);
+
+            category.Update(name, description);
+
+            Emit(new CategoryChanged());//TODO: Pass the event data
+        }
+
+        public void RemoveCategory(Guid categoryId)
+        {
+            var category = GetCategory(categoryId);
+
+            categories.Remove(category);
+
+            Emit(new CategoryRemoved());//TODO: Pass the event data
+        }
+
+        public void AddMenuItem(
+            Guid categoryId,
+            Guid menuItemId,
+            string name,
+            string description,
+            double price,
+            bool available)
+        {
+            var category = GetCategory(categoryId);
+
+            category.AddMenuItem(new MenuItem(
+                menuItemId,
+                name,
+                description,
+                price,
+                available));
+
+            Emit(new MenuItemCreated());//TODO: Pass the event data
+        }
+
+        public void UpdateMenuItem(
+            Guid categoryId,
+            Guid menuItemId,
+            string name,
+            string description,
+            double price,
+            bool available)
+        {
+            var category = GetCategory(categoryId);
+
+            category.UpdateMenuItem(new MenuItem(
+                menuItemId,
+                name,
+                description,
+                price,
+                available));
+
+            Emit(new MenuItemChanged());//TODO: Pass the event data
+        }
+
+        public void RemoveMenuItem(Guid categoryId, Guid menuItemId)
+        {
+            var category = GetCategory(categoryId);
+
+            category.RemoveMenuItem(menuItemId);
+
+            Emit(new MenuItemRemoved());//TODO: Pass the event data
+        }
+
+        private Category GetCategory(Guid categoryId)
+        {
+            var category = categories.SingleOrDefault(c => c.Id == categoryId);
+
+            if (category == null)
+                CategoryDoesNotExistException.Raise(Id, categoryId);
+
+            return category;
+        }
+
+        private void Emit(IDomainEvent domainEvent) { }
+    }
+
+#else
+
     public class Menu : AggregateRoot<Guid>
     {
         [JsonProperty("Categories")]
@@ -138,142 +282,6 @@ namespace xxAMIDOxx.xxSTACKSxx.Domain
 
             return category;
         }
-    }
-
-#elif (DynamoDb)
-    [DynamoDBTable("Menu")]
-    public class Menu
-    {
-        [JsonProperty("Categories")]
-        [DynamoDBIgnore]
-        private List<Category> categories;
-
-        // DynamoDB needs an empty constructor. In a real project you'll probably wrap your domain obj in a Dynamo DTO
-        public Menu() { }
-
-        public Menu(Guid id, string name, Guid tenantId, string description, bool enabled, List<Category> categories = null)
-        {
-            Id = id;
-            Name = name;
-            TenantId = tenantId;
-            Description = description;
-            this.categories = categories ?? new List<Category>();
-            Enabled = enabled;
-        }
-
-        [DynamoDBHashKey(AttributeName = "Id", Converter = typeof(DynamoDbGuidConverter))]
-        public Guid Id { get; private set; }
-
-        public string Name { get; private set; }
-
-        [DynamoDBProperty(AttributeName = "TenantId", Converter = typeof(DynamoDbGuidConverter))]
-        public Guid TenantId { get; private set; }
-
-        public string Description { get; private set; }
-
-        [JsonIgnore]
-        [DynamoDBIgnore]
-        public IReadOnlyList<Category> Categories { get => categories?.AsReadOnly(); private set => categories = value.ToList(); }
-
-        public bool Enabled { get; private set; }
-
-        public void Update(string name, string description, bool enabled)
-        {
-            this.Name = name;
-            this.Description = description;
-            this.Enabled = enabled;
-
-            Emit(new MenuChanged());//TODO: Pass the event data
-        }
-
-        public void AddCategory(Guid categoryId, string name, string description)
-        {
-            if (categories.Any(c => c.Name == name))
-                CategoryAlreadyExistsException.Raise(Id, name);
-
-            categories.Add(new Category(categoryId, name, description));
-
-            Emit(new CategoryCreated());//TODO: Pass the event data
-        }
-
-        public void UpdateCategory(Guid categoryId, string name, string description)
-        {
-            var category = GetCategory(categoryId);
-
-            category.Update(name, description);
-
-            Emit(new CategoryChanged());//TODO: Pass the event data
-        }
-
-        public void RemoveCategory(Guid categoryId)
-        {
-            var category = GetCategory(categoryId);
-
-            categories.Remove(category);
-
-            Emit(new CategoryRemoved());//TODO: Pass the event data
-        }
-
-        public void AddMenuItem(
-            Guid categoryId,
-            Guid menuItemId,
-            string name,
-            string description,
-            double price,
-            bool available)
-        {
-            var category = GetCategory(categoryId);
-
-            category.AddMenuItem(new MenuItem(
-                menuItemId,
-                name,
-                description,
-                price,
-                available));
-
-            Emit(new MenuItemCreated());//TODO: Pass the event data
-        }
-
-        public void UpdateMenuItem(
-            Guid categoryId,
-            Guid menuItemId,
-            string name,
-            string description,
-            double price,
-            bool available)
-        {
-            var category = GetCategory(categoryId);
-
-            category.UpdateMenuItem(new MenuItem(
-                menuItemId,
-                name,
-                description,
-                price,
-                available));
-
-            Emit(new MenuItemChanged());//TODO: Pass the event data
-        }
-
-        public void RemoveMenuItem(Guid categoryId, Guid menuItemId)
-        {
-            var category = GetCategory(categoryId);
-
-            category.RemoveMenuItem(menuItemId);
-
-            Emit(new MenuItemRemoved());//TODO: Pass the event data
-        }
-
-        private Category GetCategory(Guid categoryId)
-        {
-            var category = categories.SingleOrDefault(c => c.Id == categoryId);
-
-            if (category == null)
-                CategoryDoesNotExistException.Raise(Id, categoryId);
-
-            return category;
-        }
-
-        private void Emit(IDomainEvent domainEvent) { }
     }
 
 #endif
